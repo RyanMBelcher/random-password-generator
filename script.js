e629df57568b052cf30d7a766a5467e4e953a18a
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-11-10 17:09:36 -0500

    added conditional statements to function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+// These are the varaibale being used 
+const uppercaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "=", "_", ",", ".", "`", "~", "[", "]", "{", "}"];
 
+
+function generatePassword() {
+  let allCharacters = [];
   let passwordLength = prompt("How many characters would you like your password to contain?");
   passwordLength = Number(passwordLength);
   if (isNaN(passwordLength)) {
@@ -20,28 +27,6 @@ function generatePassword() {
   let lowercaseCharacters = confirm("Click okay to confirm if you would like to include lowercase letters");
   let numericCharacters = confirm("Click okay to confirm if you would like to include numbers");
 
-  const uppercaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  const lowercaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  const numericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "=", "_", ",", ".", "`", "~", "[", "]", "{", "}"];
-
-  let password = "";
-
-  for (let i = 0; i < passwordLength; i++) {
-    let allCharacters = uppercaseArr.concat(lowercaseArr, numericArr, specialArr);
-    let randomNum = Math.floor(Math.random() * allCharacters.length);
-    password = password + allCharacters[randomNum]
-  }
-
-
-  // let uppercaseLocation = Math.floor(Math.random() * uppercaseArr.length);
-  // let lowercaseLocation = Math.floor(Math.random() * lowercaseArr.length);
-  // let numericLocation = Math.floor(Math.random() * numericArr.length);
-  // let specialLocation = Math.floor(Math.random() * specialArr.length);
-
-
-
-
   if (
     !specialCharacters &&
     !uppercaseCharacters &&
@@ -50,6 +35,31 @@ function generatePassword() {
   ) {
     alert("Should include at least one character type!");
   }
+
+  if (specialCharacters) {
+    allCharacters = allCharacters.concat(specialArr);
+    password = password + specialCharacters[Math.floor(Math.random * specialArr.length)];
+  }
+  if (uppercaseCharacters) {
+    allCharacters = allCharacters.concat(uppercaseArr);
+    password = password + uppercaseCharacters[Math.floor(Math.random * uppercaseArr.length)];
+  }
+  if (lowercaseCharacters) {
+    allCharacters = allCharacters.concat(lowercaseArr);
+    password = password + lowercaseCharacters[Math.floor(Math.random * lowercaseArr.length)];
+  }
+  if (numericCharacters) {
+    allCharacters = allCharacters.concat(numericArr);
+    password = password + numericCharacters[Math.floor(Math.random * numericArr.length)];
+  }
+
+  let password = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNum = Math.floor(Math.random() * allCharacters.length);
+    password = password + allCharacters[randomNum]
+  }
+
 
   return password;
 }
@@ -76,42 +86,6 @@ generateBtn.addEventListener("click", writePassword);
 // let shouldIncludeLowercase;
 // let shouldIncludeNumeric;
 // let length;
-
-//   length = prompt("How many characters would you like your password to contain?");
-//   length = Number(length);
-//   // needs to make sure this can only take a number
-//   if (length < 8) {
-//     alert("Password must be at least 8 characters");
-//     return;
-//   } else if (length > 129) {
-//     alert("Password must be less than 129 characters");
-//     return;
-//   }
-
-//   if (shouldIncludeSpecialCharacters === true) {
-
-//   }
-
-//   if (shouldIncludeUppercase === true {
-
-//   }
-
-//   if (shouldIncludeLowercase === true) {
-
-//   }
-
-//   if (shouldIncludeNumeric === true) {
-
-//   }
-
-//  
-//   }
-
-// }
-
-
-
-
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
